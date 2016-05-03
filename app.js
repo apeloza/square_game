699667cd6865b833092d9ac3d96ea9711d69dfb8
apeloza <apeloza@alumni.uwo.ca>
2016-05-03 18:55:47 -0500

    base game working

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
+$('.boxcontainer').append('<div data-color="red" class="red box"></div><div data-color="blue" class="blue box"></div><div data-color="green" class="green box"></div><div data-color="peachpuff" class="peachpuff box"></div>')
 
+colorQ();
+$('.boxcontainer').on('click', '.box', checkAnswer);
 
 
 });
+
+function checkAnswer(){
+
+if ($(this).data('color') == $('.Qcontainer').data('color')){
+	$('.Acontainer').append('<p>Correct!</p>');
+	colorQ();
+} else {
+	$('.Acontainer').append('<p>Incorrect!</p>');
+	$('.boxcontainer').off('click', '.box', checkAnswer);
+
+}
+}
+function colorQ (){
+	var array = ["red", "blue", "green", "peachpuff"];
+var colorcode = randomNumber(0, array.length - 1);
+var color = array[colorcode];
+$('.Qcontainer').data('color', color);
+var containercolor = $('.Qcontainer').data('color');
+$('.Qcontainer').append('<p>' + containercolor + '</p>');
+
+
+}
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}

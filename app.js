1493215b9c8874123907e6b831834d1800df6d4d
apeloza <apeloza@alumni.uwo.ca>
2016-05-04 09:24:42 -0500

    Add score counter

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 //The following code waits for the document to be ready
 $(document).ready(function(){
 //Four boxes are added to the DOM. They do not currently have any colours.
-$('.boxcontainer').append('<div class="box1 box"></div><div class="box2 box"></div><div class="box3 box"></div><div class="box4 box"></div>')
+$('.boxcontainer').append('<div class="box1 box"></div><div class="box2 box"></div><div class="box3 box"></div><div class="box4 box"></div>');
 
 //These lines ensure that the boxes are always initialized as squares regardless of the size of the browser window.
 var $box = $('.box');
 	$box.height($box.width());
 
 //Two functions are called. The first randomizes the box colours, and the second generates our initial colour question (see below for further explanation).
+refreshArray();
 randomBoxes();
 colorQ();
 
@@ -20,18 +21,24 @@ $('.boxcontainer').on('click', '.box', checkAnswer);
 
 //From here on all code is initialized before the page has fully loaded.
 //First, an array is initialized that contains every css color name. This is global because multiple functions use it.
-	var colorsArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+	var colorsArray = [];
+  var scoreCounter = 0;
 
 //This function checks the user's answer to see if it is correct.
 function checkAnswer(){
 
 //This checks to see if the user's answer has the same color-data as the one the question is currently holding onto.
 if ($(this).data('color') == $('.Qcontainer').data('color')){
-		
+
 		//User is alerted of their correct answer, which also pauses the page
 		alert("Correct!");
-		//The page is reloaded to fetch new colours for the user.
-	document.location.reload(true);
+    scoreCounter++;
+    $('.counter').remove();
+    $('.countercontainer').append('<p class="counter">Score: ' + scoreCounter + '</p>');
+		//The game is started again
+     refreshArray();
+    randomBoxes();
+    colorQ();
 } else {
 	alert("Incorrect!");
 
@@ -52,6 +59,7 @@ $('.Qcontainer').data('color', colorsArray[colorindex]);
 
 //A variable is initialized to keep the code simple to read, and then the question container displays text on the DOM prompting the user to click a box of the corresponding color.
 var containercolor = $('.Qcontainer').data('color');
+$('.question').remove();
 $('.Qcontainer').append('<p class="question">' + containercolor + '</p>');
 
 
@@ -77,5 +85,9 @@ $('.box' + i).css('background-color', $('.box' + i).data('color'));
 	}
 
 	//Finally, the colorsArray that contained all the possible css colour names is re-written to only contain the colours that the boxes use. This is to ensure that the user is never asked for a colour that isn't present.
-colorsArray = [$('.box1').data('color'), $('.box2').data('color'), $('.box3').data('color'), $('.box4').data('color')]
+colorsArray = [$('.box1').data('color'), $('.box2').data('color'), $('.box3').data('color'), $('.box4').data('color')];
+}
+
+function refreshArray(){
+  colorsArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow"];
 }
